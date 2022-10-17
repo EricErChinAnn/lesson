4ac4716ffc9e7028a7bc0e9fea6797f1ef137003
Eric Er <er_chin_ann@hotmail.com>
2022-10-17 12:24:13 +0800

    17/10/2022

---
+++ 02_template/index.js
@@ -0,0 +1,22 @@
+const express = require(`express`);
+
+let app = express();
+
+app.set(`view engine`, `hbs`);
+
+app.get("/",(req,res)=>{
+    res.render(`index.hbs`);
+})
+
+app.listen(3000,()=>{
+    console.log(`server is up`);
+})
+
+app.get(`/greeting/:firstname/:lastname`,(req,res)=>{
+    let firstname = req.params.firstname;
+    let lastname = req.params.lastname;
+    res.render(`greeting`,{
+        "firstname":firstname,
+        "lastname":lastname
+    })
+})
